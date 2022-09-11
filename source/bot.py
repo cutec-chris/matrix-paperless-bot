f2a8e0c67978c901b99beba5fed0b971ebfa8443
Christian U <christian@ullihome.de>
2022-09-12 00:01:36 +0200

    ability to download file

--- source/bot.py
+++ source/bot.py
@@ -1,5 +1,5 @@
 from init import *
-import requests,requests.auth,mimetypes,urllib.parse,aiofiles
+import requests,requests.auth,mimetypes,urllib.parse,aiofiles,os
 loop = None
 lastsend = None
 class Server(Config):
@@ -30,7 +30,7 @@ async def tell(room, message):
     if server and server.room != room.room_id: return
     match = botlib.MessageMatch(room, message, bot, prefix)
     if match.is_not_from_this_bot() and match.prefix()\
-    and match.command("listen"):
+    and match.command("listen",case_sensitive=False):
         server = Server({
             'room': room.room_id,
             'server': match.args()[1],
@@ -41,7 +41,7 @@ async def tell(room, message):
         loop.create_task(check_server(server))
         await save_servers()
         await bot.api.send_text_message(room.room_id, 'ok')
-    elif match.command("search"):
+    elif match.command("search",case_sensitive=False):
         try:
             response = requests.get(
                 url=server.server+'/api/documents/?query="%s"' % urllib.parse.quote(match.args()[0]),
@@ -53,7 +53,7 @@ async def tell(room, message):
                 await showpage(page,server)
         except:
             pass
-    elif match.command("show"):
+    elif match.command("show",case_sensitive=False):
         try:
             response = requests.get(
                 url=server.server+'/api/documents/%s/' % match.args()[0],
@@ -64,6 +64,47 @@ async def tell(room, message):
             await showpage(page,server,True)
         except:
             pass
+    elif match.command("download",case_sensitive=False):
+        try:
+            response = requests.get(
+                url=server.server+'/api/documents/%s/download/' % match.args()[0],
+                auth=requests.auth.HTTPBasicAuth(server.user, server.password),
+                allow_redirects=False
+            )
+            file = '/tmp/%s.pdf' % match.args()[0]
+            if response.status_code == 200:
+                with open(file, 'wb') as f:
+                    f.write(response.content)
+                mime_type = mimetypes.guess_type(file)[0]
+
+                file_stat = await aiofiles.os.stat(file)
+                async with aiofiles.open(file, "r+b") as f:
+                    resp, maybe_keys = await bot.api.async_client.upload(
+                        f,
+                        content_type=mime_type,
+                        filename=os.path.basename(file),
+                        filesize=file_stat.st_size)
+                content = {
+                    "body": os.path.basename(file),
+                    "info": {
+                        "size": file_stat.st_size,
+                        "mimetype": mime_type,
+                        "thumbnail_info": None,
+                        "thumbnail_url": None
+                    },
+                    "msgtype": "m.file",
+                    "url": resp.content_uri
+                }
+
+                try:
+                    await bot.api.async_client.room_send(room.room_id,
+                                                    message_type="m.room.message",
+                                                    content=content)
+                except:
+                    print(f"Failed to send image file {file}")
+
+        except BaseException as e:
+            await bot.api.send_text_message(server.room,str(server.server)+': '+str(e))
     elif match.is_not_from_this_bot():
         try:
             if server.room != room.room_id: return
